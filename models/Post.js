eaca1a117576c2936a49be4e73617fe68e54c203
nearias3 <127260408+nearias3@users.noreply.github.com>
2024-08-13 21:52:22 -0500

    set up basic code for post.js model

---
+++ models/Post.js
@@ -0,0 +1,22 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Post extends Model {}
+
+Post.init(
+  {
+    user_id: {},
+    title: {},
+    content: {},
+    date_created: {},
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user",
+  }
+);
+
+module.exports = Post;

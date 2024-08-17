cd3df73bdfa4da7d2f80f61c27ae7d9c42a3d6a8
nearias3 <127260408+nearias3@users.noreply.github.com>
2024-08-17 14:45:00 -0500

    Filled in boilerplate code for rest of User.js model, must still add hooks and code to check password

--- models/User.js
+++ models/User.js
@@ -8,10 +8,28 @@ class User extends Model {
 
 User.init(
   {
-    id: {},
-    username: {},
-    password: {},
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: fakse,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      }
+    },
   },
+  {
+    hooks: // ???
+  }
   {
     sequelize,
     timestamps: false,

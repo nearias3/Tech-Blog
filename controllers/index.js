f9b18d93afdd6aa4ccc20fe77aab1b447371a765
nearias3 <127260408+nearias3@users.noreply.github.com>
2024-08-20 20:24:10 -0500

    Added routes and handlebars for viewing a single post, changes to other routes and handlebars accordingly

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,12 @@ const router = require("express").Router();
 const homeRoutes = require("./homeRoutes");
 const dashboardRoutes = require("./dashboardRoutes");
 const userRoutes = require("./userRoutes");
+const postRoutes = require("./postRoutes"); 
 
 router.use("/", homeRoutes);
 router.use("/dashboard", dashboardRoutes);
 router.use("/users", userRoutes);
+router.use("/", postRoutes);
 
 router.use((req, res) => {
   res.status(404).send("Page not found!");
--- controllers/postRoutes.js
+++ controllers/postRoutes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { Post, User, Comment } = require("../models");
+const withAuth = require("../utils/auth");
 
 // Get a single post by its ID
 router.get("/post/:id", async (req, res) => {
@@ -28,6 +29,23 @@ router.get("/post/:id", async (req, res) => {
       post,
       logged_in: req.session.logged_in,
     });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+// Create a new comment
+router.post("/post/:id/comment", withAuth, async (req, res) => {
+  try {
+    const newComment = await Comment.create({
+      content: req.body.content,
+      post_id: req.params.id,
+      user_id: req.session.user_id,
+      date_created: new Date(),
+    });
+
+    res.redirect(`/post/${req.params.id}`);
   } catch (err) {
     res.status(500).json(err);
   }

8370d721ae87d69ea9bb3de453c26c5c1685653c
nearias3 <127260408+nearias3@users.noreply.github.com>
2024-08-20 22:03:08 -0500

    Added some console logs to figure out why my comments aren't being added

--- controllers/postRoutes.js
+++ controllers/postRoutes.js
@@ -34,14 +34,20 @@ router.get("/post/:id", async (req, res) => {
 // Create a new comment
 router.post("/post/:id/comment", withAuth, async (req, res) => {
   try {
+    console.log("Request body:", req.body); // Delete this later
+    console.log("User ID:", req.session.user_id); // Delete this later
+
     const newComment = await Comment.create({
       content: req.body.content,
       post_id: req.params.id,
       user_id: req.session.user_id,
     });
 
+    console.log("Comment created:", newComment); // Delete this later
+
     res.redirect(`/post/${req.params.id}`);
   } catch (err) {
+    console.error("Error creating comment:", err); // Delete this later
     res.status(500).json(err);
   }
 });

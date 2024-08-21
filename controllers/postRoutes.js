234f73eba5ed28fe989c174eaa30ef45a3ddae89
nearias3 <127260408+nearias3@users.noreply.github.com>
2024-08-20 20:12:47 -0500

    Added postRoutes.js for viewing a single post

---
+++ controllers/postRoutes.js
@@ -0,0 +1,36 @@
+const router = require("express").Router();
+const { Post, User, Comment } = require("../models");
+
+// Get a single post by its ID
+router.get("/post/:id", async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+        {
+          model: Comment,
+          include: [User], 
+        },
+      ],
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: "No post found with this id!" });
+      return;
+    }
+
+    const post = postData.get({ plain: true });
+
+    res.render("single-post", {
+      post,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
